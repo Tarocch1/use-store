36ac876de8f9a71300ec71a23056fbda29cb0c5d
Tarocch1 <huiben.fang@gmail.com>
2020-01-17 01:04:03 +0800

    基本功能实现

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# use-model
-A state manager for React Hooks.
+# UseModel
+
+一个基于React Hooks的状态管理工具。
+
+## Install (Not ready)
+
+```bash
+npm install @tarocch1/use-model
+```
+
+## Usage
+
+```tsx
+import { setModel, useModel } from '@tarocch1/use-model';
+
+class CounterModel {
+  count = 0;
+  add() {
+    this.count++;
+  }
+  async addAsync() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    this.count++;
+  }
+}
+
+setModel(CounterModel);
+
+function Counter() {
+  const counter = useModel(CounterModel);
+  return (
+    <>
+      Count: {counter.count}
+      <button onClick={counter.add}>+</button>
+      <button onClick={counter.addAsync}>+ async</button>
+    </>
+  );
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { useState, useEffect, Dispatch } from 'react';
+
+export interface Model {
+  proxy: any;
+  setters: Dispatch<any>[];
+}
+export interface Models {
+  [name: string]: Model;
+}
+export interface SetModel {
+  (model: new () => any): void;
+}
+export interface UseModel {
+  <T>(model: new () => T, noRender?: boolean): T;
+}
+
+const models: Models = {};
+
+export const setModel: SetModel = model => {
+  const proxy = new Proxy(new model(), {
+    get: function(target, prop, receiver) {
+      if (typeof target[prop] === 'function') {
+        return target[prop].bind(receiver);
+      }
+      return target[prop];
+    },
+    set: function(target, prop, value) {
+      target[prop] = value;
+      const { setters } = models[model.name];
+      setters.forEach(setter => {
+        setter({ [prop]: value });
+      });
+      return true;
+    },
+  });
+  models[model.name] = { proxy, setters: [] };
+};
+
+export const useModel: UseModel = (model, noRender = false) => {
+  const [, setState] = useState();
+  const { proxy, setters } = models[model.name];
+  useEffect(() => {
+    if (noRender) return;
+    setters.push(setState);
+    return () => {
+      const index = setters.indexOf(setState);
+      setters.splice(index, 1);
+    };
+  }, []);
+  return proxy;
+};
