0f4b037187fb7d062c599813a42efdfc82b017e9
Tarocch1 <huiben.fang@gmail.com>
2020-03-07 09:46:36 +0800

    去除symbol

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,13 @@ export interface UseModel {
   <T>(Model: new () => T, noRender?: boolean): T;
 }
 
-const models: Map<symbol, Model> = new Map();
+const models: Map<new () => any, Model> = new Map();
 
 export const setModel: SetModel = Model => {
   if (typeof Model !== 'function') {
     throw new Error('Model must be a constructor.');
   }
-  const symbol = Symbol.for(Model.toString());
-  if (models.has(symbol)) return;
+  if (models.has(Model)) return;
   const proxy = new Proxy(new Model(), {
     get: function(target, prop, receiver) {
       if (typeof target[prop] === 'function') {
@@ -31,37 +30,35 @@ export const setModel: SetModel = Model => {
     },
     set: function(target, prop, value) {
       target[prop] = value;
-      const { setters } = models.get(symbol)!;
+      const { setters } = models.get(Model)!;
       setters.forEach(setter => {
         setter({ [prop]: value });
       });
       return true;
     },
   });
-  models.set(symbol, { proxy, setters: [] });
+  models.set(Model, { proxy, setters: [] });
 };
 
 export const getModel: GetModel = Model => {
   if (typeof Model !== 'function') {
     throw new Error('Model must be a constructor.');
   }
-  const symbol = Symbol.for(Model.toString());
-  if (!models.has(symbol)) {
+  if (!models.has(Model)) {
     throw new Error('Model is not set');
   }
-  return models.get(symbol)!.proxy;
+  return models.get(Model)!.proxy;
 };
 
 export const useModel: UseModel = (Model, noRender = false) => {
   if (typeof Model !== 'function') {
     throw new Error('Model must be a constructor.');
   }
-  const symbol = Symbol.for(Model.toString());
-  if (!models.has(symbol)) {
+  if (!models.has(Model)) {
     throw new Error('Model is not set');
   }
   const [, setState] = useState();
-  const { proxy, setters } = models.get(symbol)!;
+  const { proxy, setters } = models.get(Model)!;
   useEffect(() => {
     if (noRender) return;
     setters.push(setState);
